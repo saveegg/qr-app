07a3396f961b514cdb46a6df763f07a7e66717a2
Daniel <saveegg@gmail.com>
2024-01-20 22:31:50 -0800

    chore: Add private key and certificate files.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as fs from 'fs';
+
+const privateKeyPath =
+  process.env.PRIVATE_KEY_FILE || 'default/path/to/private-key.pem';
+const certificatePath =
+  process.env.CERTIFICATE_FILE || 'default/path/to/certificate.pem';
+
+const httpsOptions = {
+  key: fs.readFileSync(privateKeyPath),
+  cert: fs.readFileSync(certificatePath),
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // eslint-disable-next-line prettier/prettier
+  // const app = await NestFactory.create<NestExpressApplication>(AppModule, { httpsOptions });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    httpsOptions,
+  });
 
   app.setGlobalPrefix('api');
   const port = process.env.PORT || 3000;
