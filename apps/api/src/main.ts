69f963c0a8b7adc30d5b257559fdcd94547927a0
Daniel <saveegg@gmail.com>
2024-01-23 12:49:08 -0800

    chore: Update https option.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,25 +3,28 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as fs from 'fs';
 
-const privateKeyPath =
-  process.env.PRIVATE_KEY_FILE || 'default/path/to/private-key.pem';
-const certificatePath =
-  process.env.CERTIFICATE_FILE || 'default/path/to/certificate.pem';
-
-const httpsOptions = {
-  key: fs.readFileSync(privateKeyPath),
-  cert: fs.readFileSync(certificatePath),
-};
-
 async function bootstrap() {
-  // eslint-disable-next-line prettier/prettier
-  // const app = await NestFactory.create<NestExpressApplication>(AppModule, { httpsOptions });
+  const httpsEnabled = process.env.HTTPS_ENABLED === 'true';
+  const privateKeyPath =
+    process.env.PRIVATE_KEY_FILE || 'default/path/to/private-key.pem';
+  const certificatePath =
+    process.env.CERTIFICATE_FILE || 'default/path/to/certificate.pem';
+
+  let httpsOptions = null;
+  const port = process.env.PORT || 3000;
+
+  if (httpsEnabled) {
+    // Configure HTTPS options
+    httpsOptions = {
+      key: fs.readFileSync(privateKeyPath),
+      cert: fs.readFileSync(certificatePath),
+    };
+  }
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     httpsOptions,
   });
-
   app.setGlobalPrefix('api');
-  const port = process.env.PORT || 3000;
   await app.listen(port);
 }
 bootstrap();
